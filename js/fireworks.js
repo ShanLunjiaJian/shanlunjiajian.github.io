f4ae5ff685f9855f7fa61a4b81bf3636432f6a01
ShanLunjiaJian <1391137786@qq.com>
2022-11-04 09:16:03 +0800

    u

--- js/fireworks.js
+++ js/fireworks.js
@@ -13,6 +13,7 @@ window.addEventListener('load', () => {
   //const colors = ["#FF1461", "#18FF92", "#5A87FF", "#FBF38C"];
   //const colors = ["rgba(255,174,201)","rgba(153,217,234)","rgba(255,242,0)"];
   const colors = ["rgb(128,128,128)","rgb(159,159,159)","rgb(191,191,191)","rgb(223,223,223)","rgb(255,255,255)"];
+  const textColors = ["rgb(0,0,0)","rgb(32,32,32)","rgb(64,64,64)","rgb(96,96,96)","rgb(128,128,128)"];
 
   const setCanvasSize = debounce(() => {
     canvasEl.width = 2 * window.innerWidth,
@@ -30,7 +31,7 @@ window.addEventListener('load', () => {
   });
 
   document.addEventListener(tap, e => {
-    "sidebar" !== e.target.id && "toggle-sidebar" !== e.target.id && "A" !== e.target.nodeName && "IMG" !== e.target.nodeName && (render.play(), updateCoords(e), animateParticules(pointerX, pointerY));
+    "sidebar" !== e.target.id && "toggle-sidebar" !== e.target.id && "A" !== e.target.nodeName && "IMG" !== e.target.nodeName && (render.play(), updateCoords(e), animateParticules(e, pointerX, pointerY));
   }, !1),
     setCanvasSize(),
     window.addEventListener("resize", setCanvasSize, !1);
@@ -87,7 +88,7 @@ window.addEventListener('load', () => {
 
   function createText(text,x,y){
     const a={};
-    a.x=x,a.y=y,a.color="rgb(0,0,0)",a.alpha=1,
+    a.x=x,a.y=y,a.color=textColors[anime.random(0, textColors.length - 1)],a.alpha=1,
     a.endPos=setParticuleDirection(a),
     a.draw = function() {
       ctx.globalAlpha = a.alpha,
@@ -111,7 +112,7 @@ window.addEventListener('load', () => {
   let clickCount=0;
   let clickCountMax=0;
 
-  function animateParticules(e, t) {
+  function animateParticules(ev, e, t) {
     for (var a = createCircle(e, t), n = [], i = 0; i < numberOfParticules; i++) {
       n.push(createParticule(e, t));
     }
@@ -142,35 +143,36 @@ window.addEventListener('load', () => {
       update: a.draw
     }, 0);
 
-    clickCount++;
-    if(clickCount>clickCountMax) clickCountMax=clickCount;
-    if(clickCount>=30){
-      var texts=[];
-      for(var i=0;i<numberOfTexts;i++)
-          texts.push(createText(clickCount.toString(),e,t));
-      texts.push(createText(clickCountMax.toString(),e,t))
-      anime.timeline().add({
-        targets: texts,
-        x: function(e) {
-          return e.endPos.x;
-        },
-        y: function(e) {
-          return e.endPos.y;
-        },
-        radius: .5,
-        alpha: {
-          value: 0,
-          easing: "linear",
-          duration: 1000
-        },
-        duration: anime.random(1200, 1800),
-        easing: "easeOutExpo",
-        update: renderParticule
-      });
+    if(ev.button==0){
+      clickCount++;
+      if(clickCount>clickCountMax) clickCountMax=clickCount;
+      if(clickCount>=30){
+        var texts=[];
+        for(var i=0;i<numberOfTexts;i++)
+            texts.push(createText((clickCount/10).toString(),e,t));
+        texts.push(createText((clickCountMax/10).toString(),e,t))
+        anime.timeline().add({
+          targets: texts,
+          x: function(e) {
+            return e.endPos.x;
+          },
+          y: function(e) {
+            return e.endPos.y;
+          },
+          radius: .1,
+          alpha: {
+            value: 0,
+            easing: "linear",
+            duration: 1000
+          },
+          duration: anime.random(1200, 1800),
+          easing: "easeOutExpo",
+          update: renderParticule
+        });
+      }
+      console.log(clickCount,clickCountMax);
+      setTimeout(function(){ clickCount--; },10000);
     }
-        
-    console.log(clickCount,clickCountMax);
-    setTimeout(function(){ clickCount--; },10000);
   }
 
   function debounce(fn, delay) {
